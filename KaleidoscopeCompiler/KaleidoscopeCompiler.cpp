b1a5077d3b6620fd07a2ac9189dfee7ef72ddd81
bingo <iambingonju@gmail.com>
2019-01-25 22:25:13 +0800

    add AST class definition

--- KaleidoscopeCompiler/KaleidoscopeCompiler.cpp
+++ KaleidoscopeCompiler/KaleidoscopeCompiler.cpp
@@ -1,6 +1,7 @@
 
 #include "pch.h"
 #include <iostream>
+#include <vector>
 
 enum Token
 {
@@ -63,6 +64,72 @@ static int gettok() {
 	LastChar = getchar();
 	return ThisChar;
 }
+
+/// ExprAST - Base class for all expression nodes.
+class ExprAST {
+public:
+	virtual ~ExprAST() {}
+};
+
+/// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+	double Val;
+
+public:
+	NumberExprAST(double val) : Val(val) {}
+};
+
+/// VariableExprAST - Expression class for referrencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+	std::string Name;
+
+public:
+	VariableExprAST(std::string &name) : Name(name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+	char Op;
+	std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+	BinaryExprAST(char op, std::unique_ptr<ExprAST> lhs,
+		std::unique_ptr<ExprAST> rhs)
+		: Op(op), LHS(std::move(lhs)), RHS(std::move(rhs)) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+	std::string Callee;
+	std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+	CallExprAST(const std::string &callee, std::vector<std::unique_ptr<ExprAST>> args)
+		: Callee(callee), Args(std::move(args)) {}
+};
+
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which capture its name, and its argument names (thus implictly the number
+/// of arguments the function takes).
+class PrototypeAST : public ExprAST {
+	std::string Name;
+	std::vector<std::string> Args;
+
+public:
+	PrototypeAST(const std::string &name, std::vector<std::string> args)
+		: Name(name), Args(std::move(args)) {}
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST : public ExprAST {
+	std::unique_ptr<PrototypeAST> Proto;
+	std::unique_ptr<ExprAST> Body;
+
+public:
+	FunctionAST(std::unique_ptr<PrototypeAST> proto, std::unique_ptr<ExprAST> body)
+		: Proto(std::move(proto)), Body(std::move(body)) {}
+};
 
 int main()
 {
